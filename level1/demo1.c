92753572cca28bcf24d1f50d5bd1d9ee8184f8f9
parthi1817 <234458928+parthi1817@users.noreply.github.com>
2026-03-20 13:23:42 +0530

    First attempt without gpt

---
+++ level1/demo1.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void input(int n,float arr[]);
+int find_max_index(int n,float arr[]);
+void output(float arr[],int max_index);
+int main()
+{
+    int n, index;
+    float arr[100];
+    printf("Enter no of elements:\t\n");
+    scanf("%d",&n);
+    input(n,arr);
+    index=find_max_index(n,arr);
+    output(arr,index);
+    return 0;
+}
+void input(int n,float arr[])
+{
+    printf("Enter the array elements:\t\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+}
+int  find_max_index(int n,float arr[])
+{
+    int max_index=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]>arr[max_index])
+        max_index=i;
+    }
+    return max_index;
+}
+void output(float arr[],int max_index)
+{
+    printf("Largest number is:%d \t\n",arr[max_index]);
+    printf("Largest index is:%d\t\n",max_index);
+}
