5c044ee61d04000b14bd81abed3f059e6a76ee68
parthi1817 <234458928+parthi1817@users.noreply.github.com>
2026-03-30 19:04:06 +0530

    Learnt and executed eight program

---
+++ level1/demo8.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void input(char *str);
+void concatenate_strings(char *str1, char *str2);
+void display(char *str);
+int main()
+{
+    char str1[100],str2[100];
+    input(str1);
+    input(str2);
+    concatenate_strings(str1,str2);
+    display(str1);
+    return 0;
+
+}
+void input(char *str)
+{
+    printf("Enter a string:\t\n");
+    scanf("%s",str);
+}
+void concatenate_strings(char *str1, char *str2)
+{
+    int i=0,j=0;
+    while(str1[i]!='\0')
+    {
+        i++;
+    }
+    while(str2[j]!='\0')
+    {
+        str1[i]=str2[j];
+        i++;
+        j++;
+    }
+    str1[i]='\0';
+
+}
+void display(char *str)
+{
+    printf("The string is:%s",str);
+}
